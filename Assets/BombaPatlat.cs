b41efd941af40fc02a56622cd8f5df77db0c03c1
arifemreselvi <arifemreselvi@gmail.com>
2026-03-17 14:03:18 +0300

    Proje dosyaları eklendi.

---
+++ Assets/BombaPatlat.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BombaPatlat : MonoBehaviour
+{
+    public GameObject sahnedeSaklananIz;
+    public GameObject patlamaEfekti;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision temas)
+    {
+        if (temas.gameObject.tag == "zemin")
+        { 
+
+        Instantiate(patlamaEfekti, transform.position, transform.rotation);
+
+            sahnedeSaklananIz.SetActive(true);
+
+            Destroy(gameObject);
+        }
+    }
+}
